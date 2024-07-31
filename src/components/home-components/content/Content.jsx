b23828d818e96e37760a5d0341f7dba92d3c28a3
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 22:56:12 +0400

    animation

--- src/components/home-components/content/Content.jsx
+++ src/components/home-components/content/Content.jsx
@@ -1,12 +1,31 @@
 import "./Content.css"
+import { useEffect, useState } from "react"
 function Content(props, title, text, image) {
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        const textElements = document.querySelectorAll('.animate-text');
+        const handleScroll = () => {
+          textElements.forEach((element) => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom > 0) {
+              setShow(true);
+            } else {
+              setShow(false);
+            }
+          });
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
     return (
         <>
             <section className="content-section">
                 <div className="container flex">
                     <div className="text-container">
-                        <h3>{props.title}</h3>
-                        <p>{props.text}</p>
+                        <h3 className={`animate-text ${show ? 'show' : ''}`}>{props.title}</h3>
+                        <p className={`animate-text ${show ? 'show' : ''}`}>{props.text}</p>
                     </div>
                     <img src={props.image} alt="" />
                 </div>
