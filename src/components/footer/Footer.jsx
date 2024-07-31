aecb5b375029da233508e82fdb13d963ed2b327d
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 22:28:42 +0400

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,35 @@
 import "./Footer.css"
-function Footer()
-{
-    return(
+import logo from "../../assets/images/logo.svg"
+import facebook from "../../assets/images/facebook.svg"
+import instagram from "../../assets/images/instagra.svg"
+import twitter from "../../assets/images/twitter.svg"
+
+function Footer() {
+    return (
         <>
-        <footer>
-            
-        </footer>
+            <footer className="flex">
+                <div className="content">
+                    <img src={logo} alt="" className="logo" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.</p>
+                    <div className="info">
+                        <p>Phone: +1-543-123-4567</p>
+                        <p>example@huddle.com</p>
+                    </div>
+                    <div className="social-icons">
+                        <img src={facebook} alt="" />
+                        <img src={instagram} alt="" />
+                        <img src={twitter} alt="" />
+                    </div>
+                </div>
+                <div className="content">
+                    <h2>NEWSLETTER</h2>
+                    <p>To recieve tips on how to grow your community, sign up to our weekly newsletter. We'll never send you spam or pass on your email address</p>
+                    <div className="flex div">
+                        <input type="email" name="email" id="email" />
+                        <button>Subscribe</button>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
