d31fbcef82fca4fa36829846fc633f71be8ba7da
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 16:48:23 +0400

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 import "./Header.css";
-import logo from "../../assets/images/logo.svg";
+import logo from "../../assets/images/color_logo_transparent.svg";
 function Header()
 {
     return(
         <>
         <header>
-            <nav>
+            <nav className="flex">
                 <h1><img src={logo} alt="logo" /></h1>
                 <button>Try It Free</button>
             </nav>
