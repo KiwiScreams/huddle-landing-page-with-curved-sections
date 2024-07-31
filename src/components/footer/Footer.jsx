54fc4bb1a1c641ad6173d11a225887840ec3e4dc
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 22:39:46 +0400

    input

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -56,6 +56,7 @@ function Footer() {
                             id="email"
                             value={email}
                             onChange={handleEmailChange}
+                            className={isValid ? "" : "error-input"}
                         />
                         <button onClick={handleSubscribe}>Subscribe</button>
                         {!isValid && <p className="p" style={{ color: "red" }}>{message}</p>}
