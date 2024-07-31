f2688472abd4759a7f07ff5d56f9307bbb7764a6
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 16:33:43 +0400

    styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,15 @@
-import "./Header.css"
+import "./Header.css";
+import logo from "../../assets/images/logo.svg";
 function Header()
 {
     return(
         <>
+        <header>
+            <nav>
+                <h1><img src={logo} alt="logo" /></h1>
+                <button>Try It Free</button>
+            </nav>
+        </header>
         </>
     )
 }
